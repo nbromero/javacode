9e74f5c8dde97a678cee5046d8574c005460eb60
NRomero96 <NRomero96@10.102.233.238>
2015-02-12 15:41:54 -0600

    m

--- Assig3_2/src/PaperRockScissors.java
+++ Assig3_2/src/PaperRockScissors.java
@@ -1,16 +1,18 @@
+//Neli Romero
+//EG 1305-Spring 2015
+//Assignment 3 Program 2 
 import java.util.Scanner;
 import java.util.Random;
 
 public class PaperRockScissors {
 	public static void main(String[] args) {
+		//All the variables needed declared
 		int user;
-
 		int computer;
-
 		int scissors = 0;
 		int rock = 1;
 		int paper = 2;
-
+//Random computer generator
 		Random generator = new Random();
 		computer = generator.nextInt(3);
 		if (computer == 0)
@@ -19,11 +21,11 @@ public class PaperRockScissors {
 			computer = rock;
 		else if (computer == 2)
 			computer = paper;
-
+//Print statement for the user
 		System.out.println("scissors (0), rock (1), paper (2):");
 		Scanner scan = new Scanner(System.in);
 		user = scan.nextInt();
-
+//If else statement to determine the winner
 		if (computer == user)
 			System.out.println("It's a tie.");
 		
